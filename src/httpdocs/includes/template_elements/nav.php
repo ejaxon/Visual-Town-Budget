9b8e66b83140f3422fb54409c6329165a8f0cad7
Jtmichel <jesse@prcapps.com>
2014-05-23 12:46:46 -0400

    Nav Creation / Template Elements

---
+++ src/httpdocs/includes/template_elements/nav.php
@@ -0,0 +1,46 @@
+<ul class="nav nav-pills">
+  <li class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+      Tell Me What's New<span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+      <li>
+        <a href="#">
+          Understanding Budget Priorities
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          Explore What's Changed
+        </a>
+      </li>      
+    </ul>
+  </li>
+  <li class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+      Dropdown <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+      <li>
+        <a href="#">
+
+    </ul>
+  </li>
+  <li class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+      Dropdown <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+      ...
+    </ul>
+  </li>
+  <li class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+      Dropdown <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+      ...
+    </ul>
+  </li>      
+  ...
+</ul>
