a1355c43d11ab1c8dafddc786399ab8736059b76
ejaxon <eric@deepweave.com>
2014-05-25 10:13:37 -0400

    Killed glossary and feedback links, made external links open in new window/tab, added a "Back To Home" link

--- src/httpdocs/includes/footer.php
+++ src/httpdocs/includes/footer.php
@@ -1,8 +1,7 @@
  <div id="footer" style="font-size:1.2em">
-  <a class="link" href="/glossary">Glossary</a> - 
-  <a class="link" href="http://www.ashevillenc.gov/">City of Asheville</a> - 
-  <a class="link" href="http://codeforasheville.org/">Developed by Code for Asheville</a> -
-  <a class="link" href="http://www.goinvo.com">Originally by Involution Studios</a> -
-  <a class="link" href="/opendata"> Open Data + Code</a> -
-  <a class="link" href="https://docs.google.com/forms/d/1sc5IBg_eo8bJsTDDqU7Vk0ENdDIM13Rl6k0g7-Y07SM/viewform?usp=send_form">Feedback</a>
+  <a class="link" href="/">Back To Home</a> - 
+  <a class="link" target="_blank" href="http://www.ashevillenc.gov/">City of Asheville</a> - 
+  <a class="link" target="_blank" href="http://codeforasheville.org/">Developed by Code for Asheville</a> -
+  <a class="link" target="_blank" href="http://www.goinvo.com">Originally by Involution Studios</a> -
+  <a class="link" href="/opendata"> Open Data + Code</a>
 </div>
