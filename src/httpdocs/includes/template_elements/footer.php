b6f92b00b20b29355464ba49a8c8355e0b43bca4
Jtmichel <jesse@prcapps.com>
2014-05-26 15:07:47 -0400

    Mailchimp form update

--- src/httpdocs/includes/template_elements/footer.php
+++ src/httpdocs/includes/template_elements/footer.php
@@ -18,23 +18,23 @@
           <!-- FOOTER COLUMN 2 --> 
           <div class='span4 omega'>
             <!-- Begin MailChimp Signup Form -->
-              <link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
-              <style type="text/css">
-                #mc_embed_signup{/*background:#fff;*/ clear:left; font:14px Helvetica,Arial,sans-serif; }
-                /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-                   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-              </style>
-              <div id="mc_embed_signup">
-              <form action="http://prcapps.us6.list-manage.com/subscribe/post?u=4166aa4d0ffed76d959f3fd49&amp;id=2c84df05c0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                <label for="mce-EMAIL">Subscribe to updates on this project</label>
-                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-                  <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                  <div style="position: absolute; left: -5000px;"><input type="text" name="b_4166aa4d0ffed76d959f3fd49_2c84df05c0" tabindex="-1" value=""></div>
-                  <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-              </form>
+            <link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+            <style type="text/css">
+              #mc_embed_signup{clear:left; font:14px Helvetica,Arial,sans-serif; }
+              /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+                 We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+            </style>
+            <div id="mc_embed_signup">
+            <form action="http://codeforasheville.us8.list-manage.com/subscribe/post?u=20637c03779c7979a740ebf09&amp;id=8370aa81f4" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+              <label for="mce-EMAIL">Subscribe to our mailing list</label>
+              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;"><input type="text" name="b_20637c03779c7979a740ebf09_8370aa81f4" tabindex="-1" value=""></div>
+                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+            </form>
             </div>
-          </div>    
-          <!--End mc_embed_signup-->             
+          </div>
+            <!--End mc_embed_signup-->            
          <!-- FOOTER COLUMN 3 -->
           <div class='span4'>
             <a class="arrow-button" href="https://docs.google.com/forms/d/1jm4yubZ-_we7-GSUEcBMNjBJbrsenfk5IQy9R871zEQ/viewform?usp=send_form">
