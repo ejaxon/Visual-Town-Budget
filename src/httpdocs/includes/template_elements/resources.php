291b8e68cfd5b1e8ce56a953050287918407c0bb
Patrick Conant <conantp@gmail.com>
2014-05-26 17:05:29 -0400

    Added Google Anayltics Code

--- src/httpdocs/includes/template_elements/resources.php
+++ src/httpdocs/includes/template_elements/resources.php
@@ -3,3 +3,13 @@
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
   <script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js"></script>
   <script src="../js/hover-dropdown.js"></script>
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-51293358-1', 'avlbudget.org');
+  ga('send', 'pageview');
+
+</script>
