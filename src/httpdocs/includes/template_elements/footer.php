7ecd6a9d16c08cfcba3bbe9cac6b187fcc4f7482
ejaxon <eric@deepweave.com>
2014-05-26 15:11:21 -0400

    Removed mailchimp signup, changed message on forms signup.

--- src/httpdocs/includes/template_elements/footer.php
+++ src/httpdocs/includes/template_elements/footer.php
@@ -18,6 +18,7 @@
           <!-- FOOTER COLUMN 2 --> 
           <div class='span4 omega'>
             <!-- Begin MailChimp Signup Form -->
+<!-- COMMENT OUT SIGNUP PART 1
               <link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
               <style type="text/css">
                 #mc_embed_signup{/*background:#fff;*/ clear:left; font:14px Helvetica,Arial,sans-serif; }
@@ -28,17 +29,20 @@
               <form action="http://prcapps.us6.list-manage.com/subscribe/post?u=4166aa4d0ffed76d959f3fd49&amp;id=2c84df05c0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                 <label for="mce-EMAIL">Subscribe to updates on this project</label>
                 <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+END COMMENT OUT SIGNUP PART 1-->
                   <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+<!-- COMMENT OUT SIGNUP PART 2
                   <div style="position: absolute; left: -5000px;"><input type="text" name="b_4166aa4d0ffed76d959f3fd49_2c84df05c0" tabindex="-1" value=""></div>
                   <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
               </form>
             </div>
+END COMMENT OUT SIGNUP PART 2 -->
           </div>    
           <!--End mc_embed_signup-->             
          <!-- FOOTER COLUMN 3 -->
           <div class='span4'>
             <a class="arrow-button" href="https://docs.google.com/forms/d/1jm4yubZ-_we7-GSUEcBMNjBJbrsenfk5IQy9R871zEQ/viewform?usp=send_form">
-              <h4>I'd like to help out with the budget project!</h4>
+              <h4>I'd like to contribute to the conversation!</h4>
             </a>
           </div>                        
         </div>
