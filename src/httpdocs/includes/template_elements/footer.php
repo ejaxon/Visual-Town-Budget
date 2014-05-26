26a1bb067ecbb531798678a3f3413d9b2af8404d
ejaxon <eric@deepweave.com>
2014-05-26 14:32:58 -0400

    Fixed links to Code for Asheville and city. Changed mailing list signup to "updates for project", not just "our mailing list" since that was ambiguous.

--- src/httpdocs/includes/template_elements/footer.php
+++ src/httpdocs/includes/template_elements/footer.php
@@ -7,11 +7,11 @@
             <p>
               a collaborative web effort <br> brought to you by:
             </p>                  
-            <a href="www.codeforasheville.org">
+            <a href="http://www.codeforasheville.org" target="_blank">
               Code for Asheville
             </a>
             <span>&</span>
-            <a href="www.ashevillenc.gov">
+            <a href="http://www.ashevillenc.gov" target="_blank">
               The City of Asheville
             </a>
           </div>
@@ -26,7 +26,7 @@
               </style>
               <div id="mc_embed_signup">
               <form action="http://prcapps.us6.list-manage.com/subscribe/post?u=4166aa4d0ffed76d959f3fd49&amp;id=2c84df05c0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                <label for="mce-EMAIL">Subscribe to our mailing list</label>
+                <label for="mce-EMAIL">Subscribe to updates on this project</label>
                 <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
                   <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                   <div style="position: absolute; left: -5000px;"><input type="text" name="b_4166aa4d0ffed76d959f3fd49_2c84df05c0" tabindex="-1" value=""></div>
